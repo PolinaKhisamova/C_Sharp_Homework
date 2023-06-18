5312e076cec9db1c55fcf3db101abafd638052e5
Polina <05no1192@mail.ru>
2023-06-18 17:50:58 +0800

    1 task done

---
+++ C#_Sem2_HW/task1/Program.cs
@@ -0,0 +1,17 @@
+﻿//Программа, которая на вход принимает трехзначное число и на выходе показывает вторую цифру этого числа
+
+Console.Write("Enter a 3-digital number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+void getDigit2(int arg)
+{
+    if(arg < 0) arg = arg*(-1);
+    if(arg>99 && arg<1000)
+    {
+        Console.WriteLine("The second digit is " + arg/10%10);
+    }
+    else
+    {
+        Console.WriteLine("Entered number is NOT a 3-digital");
+    }
+}
+getDigit2(num);
