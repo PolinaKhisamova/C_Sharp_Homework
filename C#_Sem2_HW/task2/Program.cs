c8801d6cbe971003789ba9d2dc5b819a9d101756
Polina <05no1192@mail.ru>
2023-06-18 17:57:50 +0800

    2 task done

---
+++ C#_Sem2_HW/task2/Program.cs
@@ -0,0 +1,21 @@
+﻿//Программа, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+
+Console.Write("Enter a number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+void getDigit3(int arg)
+{
+    if(arg < 0) arg = arg*(-1);
+    if(arg>99)
+    {
+        while(arg/10 > 99)
+        {
+            arg = arg/10;
+        }
+        Console.WriteLine("The third digit is " + arg%10);
+    }
+    else
+    {
+        Console.WriteLine("There is NO third digit");
+    }
+}
+getDigit3(num);
