8de66087e258bd62a1062c8feacbc2d80bd1a04c
Polina <05no1192@mail.ru>
2023-06-27 16:47:48 +0800

    Seminar 3 task 3 done

---
+++ C#_Sem3_HW/task3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Программа, которая принимает на вход число N и выдает таблицу кубов чисел от 1 до N.
+
+void Cube(int numberN)
+{
+    int current = 1;
+    while(current<=numberN)
+    {
+        double result = Math.Pow(current, 3);
+        Console.Write($"{result}, ");
+        current++;
+    }
+    Console.WriteLine("\b\b.");
+}
+
+Console.Write("Enter a number: ");
+int user_number = Convert.ToInt32(Console.ReadLine());
+Cube(user_number);
