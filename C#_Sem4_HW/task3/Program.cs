df6b23dd2c1735c0e52be36c0089f3ef7ef1530f
Polina <05no1192@mail.ru>
2023-07-02 19:50:48 +0800

    seminar 4 task 3 done

---
+++ C#_Sem4_HW/task3/Program.cs
@@ -0,0 +1,28 @@
+﻿// Программа, которая задает массив из производльного количества элементов и выводит их на экран.
+
+int [] CreateNewArray(int random_size)
+{
+    int[] randomArray = new int [random_size];
+    for(int i = 0; i < random_size; i ++)
+    {
+        randomArray[i] = new Random().Next(0,10);
+    }
+    return randomArray;
+}
+
+void PrintArray (int [] array_to_print)
+{
+    Console.Write("Your array is: ");
+    for(int i = 0; i < array_to_print.Length; i ++)
+        Console.Write(array_to_print[i] + " ");
+}
+
+Console.Write("Input max. array size: ");
+int max_size = Convert.ToInt32(Console.ReadLine());
+if(max_size > 0)
+{
+    int size = new Random().Next(0, max_size+1);
+    int [] array = CreateNewArray(size);
+    PrintArray(array);
+}
+else Console.WriteLine("Incorrect max. array size.");
