e51b32c59f3cee1235dbb06b530dda2492163199
Polina <05no1192@mail.ru>
2023-07-02 19:22:00 +0800

    seminar 4 task 1 done

---
+++ C#_Sem4_HW/task1/Program.cs
@@ -0,0 +1,16 @@
+﻿int APowB (int num1, int num2)
+{
+    int res = 1;
+    for(int i = 1; i <= Math.Abs(num2); i++)
+    {
+        res*=num1;
+    }
+    return res;
+}
+
+Console.Write("Input number A: ");
+int num_a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input number B: ");
+int num_b = Convert.ToInt32(Console.ReadLine());
+int result = APowB(num_a, num_b);
+Console.WriteLine($"{num_a} power abs. ({num_b}) equals {result}");
