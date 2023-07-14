de663b36c81c44aabceca033f94a90b68271a2ac
Polina <05no1192@mail.ru>
2023-07-14 17:21:56 +0800

    seminar 7 task 1 done

---
+++ C#_Sem7_HW/task1/Program.cs
@@ -0,0 +1,27 @@
+﻿double [,] Create2DArray()
+{
+    Console.Write("How many rows? - ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.Write("How many collumns? - ");
+    int colls = Convert.ToInt32(Console.ReadLine());
+    double [,] created2DArray = new double[rows, colls];
+    for (int i = 0; i < created2DArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < created2DArray.GetLength(1); j++)
+            created2DArray [i,j] = Math.Round(new Random().Next(1,10) + new Random().NextDouble(), 2);
+    }
+    return created2DArray;
+}
+
+void Print2DArray(double [,] array2D)
+{
+    for (int i = 0; i < array2D.GetLength(0); i++)
+    {
+        for (int j = 0; j < array2D.GetLength(1); j++)
+            Console.Write(array2D[i,j] + ",  ");
+        Console.WriteLine();
+    }
+}
+
+double [,] myArray = Create2DArray();
+Print2DArray(myArray);
