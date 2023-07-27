bb6c604aef5b62197f9c7b2019924bf002e86f3d
Polina <05no1192@mail.ru>
2023-07-27 08:53:40 +0800

    seminar 9 task 1 done

---
+++ C#_Sem9_HW/task1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задать число N. Написать программу, которая выводит все натуральные числа в промежутке от N до 1.
+// Выполнить с помощью рекурсии.
+
+void ShowNums (int n)
+{
+    Console.Write(n + " ");
+    if (n > 1) ShowNums(n-1);
+}
+
+Console.Write("Enter your number: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+ShowNums(userNumber);
