dad84284f201751ea21a4f528d2f1459c59325bb
Polina <05no1192@mail.ru>
2023-07-27 16:02:08 +0800

    seminar 9 task 3 done

---
+++ C#_Sem9_HW/task3/Program.cs
@@ -0,0 +1,11 @@
+﻿// Функция Аккермана
+
+int AckFunc (int n, int m)
+{
+    if (n == 0) return m+1;
+    else if (m == 0) return AckFunc(n-1, 1);
+    else return AckFunc(n-1, AckFunc(n, m-1));
+}
+
+int res = AckFunc(3,3);
+Console.Write(res);
