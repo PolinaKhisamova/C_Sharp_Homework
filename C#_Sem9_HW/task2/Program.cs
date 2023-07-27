6d86fe40f7acfeb4258d5c6b988377348f13c730
Polina <05no1192@mail.ru>
2023-07-27 15:40:12 +0800

    seminar 9 task 2 done

---
+++ C#_Sem9_HW/task2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задать числи M и N. Написать программу, которая найдет сумму натуральных элементов от M до N.
+
+void SumOfElements (int m, int n, ref int s)
+{
+    if (n != m) SumOfElements(Math.Min(m,n), Math.Max(m,n)-1, ref s);
+    s += Math.Max(m, n);
+}
+
+int Sum (int m, int n)
+{
+    int sum = 0;
+    SumOfElements(m, n, ref sum);
+    return sum;
+}
+
+int res = Sum(4, 6);
+Console.Write ("Summ of elements is " + res);
